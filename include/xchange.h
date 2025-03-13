87834786815522cc4965f2644715567df7a30c0c
Attila Kovacs <attipaci@gmail.com>
2025-03-13 14:26:29 +0100

    v1.0.0-rc6

--- include/xchange.h
+++ include/xchange.h
@@ -27,7 +27,7 @@
 #define XCHANGE_PATCHLEVEL     0
 
 /// Additional release information in version, e.g. "-1", or "-rc1".
-#define XCHANGE_RELEASE_STRING "-rc5"
+#define XCHANGE_RELEASE_STRING "-rc6"
 
 
 #ifdef str_2
